14d22477b1443e2ad76ee27fd2c511d4b3f6f087
pedro <pedropini@raptortech.com.au>
2022-02-23 12:34:17 +0800

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
+import React from 'react';
+import { SafeAreaView, View, StatusBar, Platform, StyleSheet, Dimensions, ScrollView } from 'react-native';
+import * as eva from '@eva-design/eva';
+import { ApplicationProvider, Layout, Text, IconRegistry, List, ListItem } from '@ui-kitten/components';
+import { TopNavigationImageTitleShowcase } from './src/components/menu/index';
+import { EvaIconsPack } from '@ui-kitten/eva-icons';
+import { TabBarAccessoriesShowcase } from './src/components/tabBar/index';
+import { LineChartExample, ProcessRingExample } from './src/components/charts/index'
+import { DrawerGroupsShowcase } from './src/components/drawer/index'
+import { ViewPagerSimpleUsageShowcase } from './src/components/viewpaper/index'
+import { ToggleStatusShowcase } from './src/components/toogle'
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 10
+  }
 });
+
+export default () => (
+  <SafeAreaView style={{flex: 1}}>
+   
+    <View style={styles.container}>
+    <IconRegistry icons={EvaIconsPack} />
+    <ApplicationProvider {...eva} theme={eva.light}>        
+      <TopNavigationImageTitleShowcase />
+      
+      <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+      <ProcessRingExample/>
+        <LineChartExample/>
+        <DrawerGroupsShowcase/>
+        <ViewPagerSimpleUsageShowcase/>
+        <TabBarAccessoriesShowcase/>
+        <ToggleStatusShowcase/>
+        </ScrollView>
+    </ApplicationProvider>
+    
+    </View>
+  
+    
+  </SafeAreaView>
+  
+);
---
+++ src/components/charts/index.js
@@ -0,0 +1,92 @@
+import React from 'react';
+import { Dimensions, ScrollView,Text, View, StyleSheet } from 'react-native';
+
+import {
+    LineChart,
+    BarChart,
+    PieChart,
+    ProgressChart,
+    ContributionGraph,
+    StackedBarChart
+  } from "react-native-chart-kit";
+
+
+
+export function LineChartExample(){
+    return(
+        <LineChart
+    data={{
+      labels: ["January", "February", "March", "April", "May", "June"],
+      datasets: [
+        {
+          data: [
+            Math.random() * 100,
+            Math.random() * 100,
+            Math.random() * 100,
+            Math.random() * 100,
+            Math.random() * 100,
+            Math.random() * 100
+          ]
+        }
+      ]
+    }}
+    width={Dimensions.get("window").width} // from react-native
+    height={220}
+    yAxisLabel="$"
+    yAxisSuffix="k"
+    yAxisInterval={1} // optional, defaults to 1
+    chartConfig={{
+      backgroundColor: "#e26a00",
+      backgroundGradientFrom: "#fb8c00",
+      backgroundGradientTo: "#ffa726",
+      decimalPlaces: 2, // optional, defaults to 2dp
+      color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+      labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+      style: {
+        borderRadius: 16
+      },
+      propsForDots: {
+        r: "6",
+        strokeWidth: "2",
+        stroke: "#ffa726"
+      }
+    }}
+    bezier
+    style={{
+      marginVertical: 8,
+      borderRadius: 16
+    }}
+  />
+    )
+}
+
+
+export function ProcessRingExample(){
+  const data = {
+    labels: ["Swim", "Bike", "Run"], // optional
+    data: [0.4, 0.6, 0.8]
+  };
+  const screenWidth = Dimensions.get("window").width;
+  const chartConfig = {
+    backgroundGradientFrom: "#1E2923",
+    backgroundGradientFromOpacity: 0,
+    backgroundGradientTo: "#08130D",
+    backgroundGradientToOpacity: 0.5,
+    color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
+    strokeWidth: 2, // optional, default 3
+    barPercentage: 0.5,
+    useShadowColorFromDataset: false // optional
+  };
+
+  return(
+    <ProgressChart
+  data={data}
+  width={screenWidth}
+  height={220}
+  strokeWidth={16}
+  radius={32}
+  chartConfig={chartConfig}
+  hideLegend={false}
+/>
+  )
+}
